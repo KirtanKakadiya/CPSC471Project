ceb9ff131ddc6c762ec881505e7132fe6d5bf91e
KirtanKakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2024-02-12 22:17:15 -0700

    made changes to the navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,13 +6,13 @@ import './NavBar.css'
 
 export default function NavBar(){
     return(
-        <nav className='nav-container'>
+        <nav className='sidebar'>
             <ul>
-                <li>
-                    <IoHomeSharp />
+                <li className='sidebar-links'>
+                    <IoHomeSharp className='sidebar-icons'/>
                     <a href='/home'>Home</a>
                 </li>
-                <li>
+                <li className='sidebar-links'>
                     <LuHistory />
                     <a href='/history'>History</a>
                 </li>
