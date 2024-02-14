09d4e68b244944fbcf77b1e0d99b9a021e38e83a
KirtanKakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2024-02-13 21:20:41 -0700

    small changes to room number

--- client/src/components/StudyRoom.jsx
+++ client/src/components/StudyRoom.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import '../assets/styling/StudyRoom.css';
 
 export default function StudyRoom(props){
-
+    const roomNumber = props.roomNumber;
     return(
         <div className='content-wrapper-rooms'>
             <button className='room-button'>
-                Room 1
+                {roomNumber}
                 <div className='room-info'>
                     Info about Room
                     <button>
