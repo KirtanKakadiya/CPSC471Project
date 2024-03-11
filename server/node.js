617dccfe891475e3e72f1b0ebdcb9ac8d07d6f41
KirtanKakadiya <kirtan.kakadiya@gmail.com>
2024-03-11 12:01:55 -0600

    created routes for user controller

--- server/node.js
+++ server/node.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3001
 
+
+//Controllers
+const userController = require('./controllers/user.js');
+const roomController = require('./controllers/room.js');
+const courseController = require('./controllers/courses.js');
+const bookingController = require('./controllers/booking.js');
 // const databaseConnection = require('./database/model.js');
 
-app.get('/', function(req, res){
-    res.send('Hello World!');
-});
+app.use(express.json());
+
+//Routes
+app.use('user', userController);
+app.use('room', roomController);
+app.use('courses', courseController);
+app.use('booking', bookingController);
 
 app.listen(port, function(){
-    console.log('Example app listening on port 3001!');
+    console.log(`Server listening on port ${port}`);
 });
